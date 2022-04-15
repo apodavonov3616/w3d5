275adb86bf909edc79b38beb4ed0d6a36b31594d
apodavonov3616 <apodavonov@gmail.com>
2022-04-15 13:59:20 -0400

    finished dfs

--- polynodetree/lib/00_tree_node.rb
+++ polynodetree/lib/00_tree_node.rb
@@ -13,8 +13,29 @@ class PolyTreeNode
     node.children << self if node && !node.children.include?(self) 
   end
   
+  def add_child(node)
+    self.children << node if !children.include?(node)
+    node.parent = self
+  end
+
+  def remove_child(node)
+    raise 'node is not a child' if !self.children.include?(node)
+    node.parent = nil
+  end
+
+  def dfs(target_value)
+    node = self
+    return node if node.value == target_value
+    node.children.each do |ele|
+      possible_node = ele.dfs(target_value)
+      return possible_node if possible_node
+    end
+    nil
+  end
+
   def inspect
     "#<Stack:#{self.value}>"
   end
 
 end
+
